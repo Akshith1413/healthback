295315ab69a075d856b3f61929fcf981368462e0
Akshith <ravulaakshith1@gmail.com>
2025-06-28 01:54:32 +0530

    Initial commit

--- server.js
+++ server.js
@@ -14,13 +14,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// MongoDB connection
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(() => console.log("Connected to MongoDB"))
-.catch(err => console.error("MongoDB connection error:", err));
+// MongoDB connection (recommended for Mongoose 6+)
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("Connected to MongoDB"))
+    .catch(err => console.error("MongoDB connection error:", err));
 
 // Default route
 app.get("/", (req, res) => {
